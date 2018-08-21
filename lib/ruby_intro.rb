abac5922611340377c3acd7b4fabb92e45803a16
Javier Novella <fnovella@gmail.com>
2018-08-21 10:33:00 +0000

    HW1 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,89 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+# Define a method sum(array) that takes an array of integers as an argument and
+# returns the sum of its elements. For an empty array it should return zero.
+
+def sum(arr)
+  arr.inject(0,:+)
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+# Define a method max_2_sum(array) which takes an array of integers as an
+# argument and returns the sum of its two largest elements. For an empty array
+# it should return zero. For an array with just one element, it should return
+# that element.
+
+def max_2_sum(arr)
+  return 0 if arr.empty?
+  return arr.first if arr.length == 1
+  arr.sort!
+  arr[-1] + arr[-2]
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+# Define a method sum_to_n?(array, n) that takes an array of integers and an
+# additional integer, n, as arguments and returns true if any two elements in
+# the array of integers sum to n. sum_to_n?([], n) should return false for any
+# value of n, by definition.
+
+def sum_to_n?(arr, n)
+  return false if arr.empty?
+  arr.combination(2).any? { |n1, n2| n1 + n2 == n }
 end
 
 # Part 2
 
+# Define a method hello(name) that takes a string representing a name and
+# returns the string "Hello, " concatenated with the name
+
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+# Define a method starts_with_consonant?(s) that takes a string and returns
+# true if it starts with a consonant and false otherwise. (For our purposes, a
+# consonant is any letter other than A, E, I, O, U.) NOTE: be sure it works for
+# both upper and lower case and for nonletters!
+
+def starts_with_consonant?(s)
+  return false if s.empty?
+  /[b-df-hj-np-tv-z]/ =~ s[0].downcase
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+# Define a method binary_multiple_of_4?(s) that takes a string and returns true
+# if the string represents a binary number that is a multiple of 4. NOTE: be
+# sure it returns false if the string is not a valid binary number!
+
+def binary_multiple_of_4?(s)
+  return false unless s =~ /^[0-1]+$/
+  s.to_i % 4 == 0
 end
 
 # Part 3
 
+# Define a class BookInStock which represents a book with an ISBN number, isbn,
+# and price of the book as a floating-point number, price, as attributes.
+
+# The constructor should accept the ISBN number (a string, since in real life
+# ISBN numbers can begin with zero and can include hyphens) as the first
+# argument and price as second argument, and should raise ArgumentError (one of
+# Ruby's built-in exception types) if the ISBN number is the empty string or if
+# the price is less than or equal to zero. Include the proper getters and
+# setters for these attributes.
+
+# Include a method price_as_string that returns the price of the book formatted
+# with a leading dollar sign and two decimal places, that is, a price of 20
+# should format as "$20.00" and a price of 33.8 should format as "$33.80".
+
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$%.2f" % [price]
+  end
 end
